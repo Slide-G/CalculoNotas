075408ab7d2137d52cac4212950ba8df5043f0b4
Diego <diegogom2004@gmail.com>
2024-10-27 13:19:19 +0100

    Tercer commit

--- controllers/notasView.diego.controller.php
+++ controllers/notasView.diego.controller.php
@@ -39,4 +39,69 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
         }
     }
+    function validateJsonStructure($data)
+    {
+        foreach ($data as $module) {
+            if (!isset($module['nombre']) || !isset($module['alumnos']) || !is_array($module['alumnos'])) {
+                return false;
+            }
+            foreach ($module['alumnos'] as $student) {
+                if (!isset($student['nombre']) || !isset($student['notas']) || !is_array($student['notas'])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    // Función para procesar notas y calcular los resultados
+    function processGrades($data) {
+        $result = [];
+        // Inicializamos contadores y variables
+        $approvedStudents = [];
+        $failedStudents = [];
+        $promotedStudents = [];
+        $notPromotedStudents = [];
+
+        foreach ($data as $module) {
+            $moduleName = $module['nombre'];
+            $result[$moduleName] = [
+                'nota_media' => 0,
+                'num_suspensos' => 0,
+                'num_aprobados' => 0,
+                'nota_max' => 0,
+                'mejor_alumno' => '',
+                'nota_min' => PHP_INT_MAX,
+                'peor_alumno' => '',
+            ];
+
+            $totalGrades = 0;
+            $totalScore = 0;
+
+            foreach ($module['alumnos'] as $student) {
+                $studentName = $student['nombre'];
+                $grades = $student['notas'];
+                $totalGrades += count($grades);
+                $totalScore += array_sum($grades);
+
+                $maxGrade = max($grades);
+                $minGrade = min($grades);
+                $avgGrade = array_sum($grades) / count($grades);
+
+                if ($maxGrade > $result[$moduleName]['nota_max']) {
+                    $result[$moduleName]['nota_max'] = $maxGrade;
+                    $result[$moduleName]['mejor_alumno'] = $studentName;
+                }
+                if ($minGrade < $result[$moduleName]['nota_min']) {
+                    $result[$moduleName]['nota_min'] = $minGrade;
+                    $result[$moduleName]['peor_alumno'] = $studentName;
+                }
+
+                if ($avgGrade >= 5) {
+                    $result[$moduleName]['num_aprobados']++;
+                } else {
+                    $result[$moduleName]['num_suspensos']++;
+                }
+            }
+
+
 
